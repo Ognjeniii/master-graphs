ce92330e13a1e2dfee929663f502d7b827e073f8
Ognjen Ilić <ognjeni@telekom.rs>
2024-10-16 16:40:47 +0200

    chng

--- src/RandomGraphs/RandomAdjListOptimized.java
+++ src/RandomGraphs/RandomAdjListOptimized.java
@@ -13,32 +13,31 @@ public class RandomAdjListOptimized {
 
     private static Random random = new Random();
 
-    @SuppressWarnings("unchecked")
     public RandomAdjListOptimized(int V, double num) {
-        this.V = V;
-        long e = Math.round((V * (V - 1) / 2.0) * num);
-        E = (int) e;
+        this.V = V; // postavljanje broja čvorova
+        long e = Math.round((V * (V - 1) / 2.0) * num); // za dobijanje broja grana
+        E = (int) e; // postavljanje broja grana
 
-        graph = new ArrayList[V];
+        graph = new ArrayList[V]; // zauzimanje mesta za graf
         for(int i = 0; i < V; i++) {
             graph[i] = new ArrayList<>();
         }
 
-        int counter = 0;
-        while(counter < E) {
+        int counter = 0; // ovom promenljivom vodimo eviedenciju o broju grana
+        while (counter < E) {
             int u = random.nextInt(V);
             int v = random.nextInt(V);
-            if(u == v) continue; // izbegavajte samopetlje
-            boolean exists = false;
-            for(Node node : graph[u]) {
-                if(node.getVertex() == v) {
+            if (u == v) continue; // izbegavanje samopetlje
+            boolean exists = false; // pomoćna promenljiva
+            for (Node node : graph[u]) {
+                if (node.getVertex() == v) { // ako već postoji grana od u do v
                     exists = true;
                     break;
                 }
             }
-            if(!exists) {
-                int weight = random.nextInt(7) + 3;
-                graph[u].add(new Node(v, weight));
+            if (!exists) {
+                int weight = random.nextInt(7) + 3; // generiše se težina
+                graph[u].add(new Node(v, weight)); // dodaje se grana
                 counter++;
             }
         }
